e2d6ec83f4215ae6b1fb451b40aa734be345bb50
stefanzan <zantao@nii.ac.jp>
2015-09-25 16:21:46 +0900

    Add a module to find type/value constructor name in Template Haskell

---
+++ Haskell/src/Generics/BiGUL/TH.hs
@@ -0,0 +1,19 @@
+module Generics.BiGUL.TH where
+import Language.Haskell.TH
+import Control.Monad
+
+
+-- Help function: lookup type constructor name by a given string.
+lookupTypeConstructor :: TypeConstructor -> ErrorMessage -> Q Name
+lookupTypeConstructor name errMsg = lookupTypeName name >>= maybe (fail errMsg) return
+
+-- Help function: lookup value constructor name by a given string.
+lookupValueConstructor :: ValueConstructor -> ErrorMessage -> Q Name
+lookupValueConstructor name errMsg = lookupValueName name >>= maybe (fail errMsg) return
+
+-- ["Generic", "K1", "U1", ":+:", ":*:", "Rep"]
+type TypeConstructor = String
+type ValueConstructor = String 
+type ErrorMessage = String
+genericTypeCTuple :: [TypeConstructor] -> [ValueConstructor] -> ErrorMessage -> Q ([Name], [Name])
+genericTypeCTuple typeCList valueCList errMsg = liftM2 (,) (mapM (flip lookupTypeConstructor errMsg) typeCList) (mapM (flip lookupValueConstructor errMsg) valueCList)
