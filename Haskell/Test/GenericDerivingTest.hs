a853d0d0a56dab3ad81e711e30aad64534574d9e
stefanzan <zantao@nii.ac.jp>
2015-09-25 21:05:26 +0900

    Generic Instance Generation with Template Haskell.

---
+++ Haskell/Test/GenericDerivingTest.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE TypeOperators, TypeFamilies, FlexibleContexts, DeriveGeneric, TemplateHaskell, TupleSections  #-}
+module GenericDerivingTest where
+
+import GHC.Generics
+import GHC.InOut
+import Language.Haskell.TH
+import Control.Monad
+import Generics.BiGUL.TH
+
+data Bookmark = Bookmark String String Bool
+              | Folder String [Bookmark]
+              | Sep 
+              deriving (Show)
+
+
+deriveBiGULGeneric ''Bookmark
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
