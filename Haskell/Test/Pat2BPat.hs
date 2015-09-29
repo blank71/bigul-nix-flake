8ca0598dc3add34aeb7883a31a405c958aa98014
LiuLi <liuli@LiuLideMacBook-Pro.local>
2015-09-29 17:49:42 +0900

    generalize function mkNPat

--- Haskell/Test/Pat2BPat.hs
+++ Haskell/Test/Pat2BPat.hs
@@ -1,64 +1,98 @@
 {-# LANGUAGE TemplateHaskell #-}
 
-module Pat2BPat(branch, normal',adaptive,normal) where
+module Pat2BPat(branch, normal',adaptive,normal,mkNPat) where
 
 import Generics.BiGUL
 import Generics.BiGUL.AST
 import Language.Haskell.TH as TH
 import Generics.BiGUL.TH
 
+
+
 pat2BPat :: TH.Pat -> Q TH.Exp
 pat2BPat p = do
-  pat <- mkNPat p
+  pat <- mkNPat p "P"
   maybeConCaseVB <- lookupValueName "Generics.BiGUL.AST.CaseVBranch"
   case maybeConCaseVB of
     Nothing -> fail $ "cannot find constructor CaseVBranch"
     Just conCaseVB ->
       return $ ConE conCaseVB `AppE` pat
 
-mkNPat :: TH.Pat -> Q TH.Exp
+mkNPat :: TH.Pat -> String -> Q TH.Exp
+
+mkNPat (LitP c) patTag  = do 
+  (_, [gconst]) <- lookupNames [] ["Generics.BiGUL.AST." ++ patTag ++ "Const"] "cannot find constructors GConst from Generics.BiGUL.AST."
+  return $ ConE gconst `AppE` LitE c
+
+
+
+
+-- user defined datatypes
+mkNPat (ConP name ps) patTag = do
+      ConP name' [] <- [p| [] |]
+      if name == name' && ps == []
+      then do 
+           (_, [gconst]) <- lookupNames [] ["Generics.BiGUL.AST." ++ patTag ++ "Const"] "cannot find constructors GConst from Generics.BiGUL.AST."
+           return $ ConE gconst `AppE` (ListE []) 
+      else do
+           lrs <- lookupLRs name
+           conInEither <- mkConstrutorFromLRs lrs patTag
+           pes         <- case ps of
+                           [] -> mkNPat (ListP []) patTag 
+                           _  -> mkNPat (TupP ps)  patTag
+           return $ conInEither pes
+
 -- pattern ([]) ---> PConst []
-mkNPat (ConP name []) = do
-  (_, [pconst]) <- lookupNames [] ["Generics.BiGUL.AST.PConst"] "cannot find constructors PConst from Generics.BiGUL.AST."
-  ConP name' [] <- [p| [] |]
+--mkNPat (ConP name []) patTag = do
+--  (_, [gconst]) <- lookupNames [] ["Generics.BiGUL.AST." ++ patTag ++ "Const"] "cannot find constructors GConst from Generics.BiGUL.AST."
+--  ConP name' [] <- [p| [] |]
+--  if name == name'
+--    then return $ ConE gconst `AppE` (ListE [])
+--    else fail $ "constructors mismatch: " ++ nameBase name ++ " and " ++ nameBase name'
+
+-- pattern ([_]) or (_ : []) singleton list ---> PIn (PRight (PProd PVar (PConst [])
+-- (_ : []) is handled by "InfixP WildP name pats" with pats = ConP GHC.Types.[] []
+mkNPat (ListP []) patTag = [p| [] |] >>= flip mkNPat  patTag 
+                           
+-- pattern ([_, _]). (_ : _ : []) is handled by "InfixP WildP name pats"
+mkNPat (ListP (p:xs)) patTag = do
+  hexp <- mkNPat p patTag
+  rexp <- mkNPat (ListP xs) patTag
+  (_, [gelem]) <- lookupNames [] ["Generics.BiGUL.AST." ++ patTag ++ s | s <- ["Elem"]] "cannot find type constructors GElem from Generics.BiGUL.AST."
+  return $ (ConE gelem `AppE` hexp) `AppE` rexp
+
+-- pattern (_:_) ---> PIn (PRight (PProd PVar PVar
+mkNPat (InfixP pl name pr) patTag = do
+  lpat <- mkNPat pl patTag
+  rpat <- mkNPat pr patTag
+  (_, [gelem]) <- lookupNames [] ["Generics.BiGUL.AST." ++ patTag ++ s | s <- ["Elem"]] "cannot find type constructors GElem from Generics.BiGUL.AST."
+  ConE name' <- [| (:) |]
   if name == name'
-    then return $ ConE pconst `AppE` (ListE [])
+    then return $ (ConE gelem `AppE` lpat) `AppE` rpat
     else fail $ "constructors mismatch: " ++ nameBase name ++ " and " ++ nameBase name'
 
--- pattern ([_]) or (_ : []) singleton list ---> POut (PRight (PProd PVar (PConst [])
--- (_ : []) is handled by "InfixP WildP name pats" with pats = ConP GHC.Types.[] []
-mkNPat (ListP [WildP]) = [p| _:[] |] >>= mkNPat
-
--- pattern ([_, _]). (_ : _ : []) is handled by "InfixP WildP name pats"
-mkNPat (ListP (WildP:xs)) = do
-  rexp <- mkNPat (ListP xs)
-  (_, [pvar, pprod, pright, pout]) <- lookupNames [] ["Generics.BiGUL.AST." ++ s | s <- ["PVar", "PProd", "PRight", "POut"]] "cannot find type constructors PVar PProd PRight POut from Generics.BiGUL.AST."
-  return $ ConE pout `AppE` (ConE pright `AppE` (ConE pprod `AppE` (ConE pvar) `AppE` rexp))
-
--- pattern (_:_) ---> POut (PRight (PProd PVar PVar
-mkNPat (InfixP WildP name pats) =
-  case pats of
-    VarP _ -> fail $ "cannot use variables in list patterns. use wildcast instead"
-    _      -> do
-      rpat <- mkNPat pats
-      (_, [pvar, pprod, pright, pout]) <- lookupNames [] ["Generics.BiGUL.AST." ++ s | s <- ["PVar", "PProd", "PRight", "POut"]] "cannot find type constructors PVar PProd PRight POut from Generics.BiGUL.AST."
-      ConE name' <- [| (:) |]
-      if name == name'
-        then return $ ConE pout `AppE` (ConE pright `AppE` (ConE pprod `AppE` (ConE pvar) `AppE` rpat))
-        else fail $ "constructors mismatch: " ++ nameBase name ++ " and " ++ nameBase name'
-mkNPat (WildP) = lookupNames [] ["Generics.BiGUL.AST.PVar"]  "cannot find constructors PVar from Generics.BiGUL.AST." >>= \(_, [pvar]) -> return $ ConE pvar
 
 -- pattern (x,y,z)) ---> PProd (PLeft)
-mkNPat (TupP [p]) = mkNPat p
-mkNPat (TupP (p:ps)) = do
-  lexp <- mkNPat p
-  rexp <- mkNPat (TupP ps)
-  (_, [pprod, pleft, pright]) <- lookupNames [] ["Generics.BiGUL.AST." ++ s | s <- ["PProd", "PLeft", "PRight"]] "cannot find constructors PProd PLeft PRight from Generics.BiGUL.AST."
-  return ((ConE pprod `AppE` lexp) `AppE` rexp)
+mkNPat (TupP [p]) patTag = mkNPat p patTag
+mkNPat (TupP (p:ps)) patTag = do
+  lexp <- mkNPat p patTag
+  rexp <- mkNPat (TupP ps) patTag
+  (_, [gprod]) <- lookupNames [] ["Generics.BiGUL.AST." ++ patTag ++ s | s <- ["Prod"]] "cannot find constructors GProd from Generics.BiGUL.AST."
+  return ((ConE gprod `AppE` lexp) `AppE` rexp)
 
+
+mkNPat (WildP) "P" = do (_, [pvar]) <- lookupNames [] ["Generics.BiGUL.AST.PVar"]  "cannot find constructors PVar from Generics.BiGUL.AST." 
+                        return $ ConE pvar
+mkNPat (WildP) "U" = do (_, [uvar, skip]) <- lookupNames [] ["Generics.BiGUL.AST." ++ s | s <- ["UVar", "Skip"]] "cannot find constructors UVar Skip from Generics.BiGUL.AST."
+                        return $ ConE uvar `AppE` ConE skip
+mkNPat (WildP) "R" = do (_, [rvar]) <- lookupNames [] ["Generics.BiGUL.AST." ++ s | s <- ["RVar"]] "cannot find constructors RVar from Generics.BiGUL.AST."
+                        return $ ConE rvar
 -- pattern (x) ---> PVar
-mkNPat (VarP x) = fail $ "please do not use variables in patterns. use wildcast(_) instead."
-mkNPat _ = fail $ "pattern not handled yet."
+mkNPat (VarP x) "P" =  fail $ "please do not use variables in Ppatterns. use wildcast(_) instead."
+mkNPat (VarP x) "U" =  mkNPat WildP "U"
+mkNPat (VarP x) "R" =  mkNPat WildP "R"
+
+mkNPat _ patTag = fail $ "pattern not handled yet."
 
 
 branch :: Q TH.Pat -> Q TH.Exp
--- Haskell/src/Generics/BiGUL/TH.hs
+++ Haskell/src/Generics/BiGUL/TH.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE TupleSections, DeriveDataTypeable #-}
 module Generics.BiGUL.TH
   (
-    ConTag(..), contag, ConTagSeq(..), lookupNames, constructLRs, lookupLRs, -- for temporary usage, will be deleted later.
+    ConTag(..), contag, ConTagSeq(..), lookupNames, constructLRs, lookupLRs, mkConstrutorFromLRs, -- for temporary usage, will be deleted later.
     deriveBiGULGeneric
   )
 where
@@ -142,3 +142,7 @@ lookupLRs conName = do
   return $ constructLRs (length cons) !!
              fromJust (List.findIndex (== conName) (map (\(NormalC n _) -> n) cons))
 
+
+mkConstrutorFromLRs :: [ConTag] -> String -> Q (Exp -> Exp)
+mkConstrutorFromLRs lrs patTag = do (_, [gin, gleft, gright]) <- lookupNames [] [ "Generics.BiGUL.AST." ++ patTag ++ s | s <- ["In", "Left", "Right"] ] "cannot find data constructors from Generic.BiGUL.AST"
+                                    return $ foldl (.) (AppE (ConE gin)) (map (AppE . ConE . contag gleft gright) lrs)
