33bf95525806fd54b8fb8fb8dbb01bf3b1a4bdd6
Jorge Mendes <jorgecunhamendes@gmail.com>
2016-03-19 11:47:04 +0000

    Small UNSAFE script to check the examples.
    Be careful when running running the script! It assumes everything is
    syntactically correct, and doesn't check the values passed to the
    terminal!
    
    This script was made to test the examples on my system. It assumes two
    repositories: one on the branch of the paper, and another on the branch
    of the code. Also, I'm using stack in order to use a recent GHC and
    a stable set of libraries.
    
    It shouldn't be too hard to modify it to run on other systems, but this
    isn't my goal at the moment. It was quickly developed to check if I
    didn't make any mistake when writing the examples.

---
+++ ICFP16/check.pl
@@ -0,0 +1,86 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use IO::Handle qw(flush);
+
+# environment
+my $haskelldir = '../../bigul-clone/Haskell';
+my $paperdir = `pwd`;
+chomp $paperdir;
+
+# execution
+my $eval = 'stack ghc --';
+my $extensions = '-XTemplateHaskell -XTypeFamilies -XTypeOperators';
+
+# contents
+my $prompt = qr|@@>@@|;
+my $newline = qr|@@\\lstcontinueline@@|;
+
+# result
+my $examples = 0;
+my $wrong = 0;
+
+# internals
+my $state = 0;
+my $expr = '';
+my $i = 0;
+
+# file parsing
+while (<STDIN>) {
+  chomp;
+  my $line = $_;
+ 
+  if ($line =~ /^$prompt\s*(.*?)\s*$newline\s*$/) {
+    $expr = $1;
+    $state = 2;
+  } elsif ($state == 2 and $line =~ /\s*(.*?)\s*$newline\s*$/) {
+    $expr .= " $1";
+  } elsif ($state == 2 and $line =~ /^\s*(.*?)\s*$/) {
+    $expr .= " $1";
+    $state = 1;
+  } elsif ($line =~ /^$prompt\s*(.*)$/) {
+    $expr = $1;
+    $state = 1;
+  } elsif ($state == 1) {
+    runExample($expr, $line, $i);
+    $expr = '';
+    $state = 0;
+  }
+
+  $i++;
+}
+
+# show final result
+if (not $wrong) {
+  print "Found $examples examples, all working.\n";
+} else {
+  print "Found $examples examples, $wrong not working.\n";
+}
+
+# run example and check if the result is the one in the example
+sub runExample {
+  my $e = shift;
+  my $l = shift;
+  my $i = shift;
+  $examples++;
+  # informative message
+  print "Checing \"$e\"";
+  STDOUT->flush();
+  # run example
+  my $result = `cd $haskelldir; $eval $extensions -e "$e" $paperdir/icfp16.lhs`;
+  chomp $result;
+  # check result
+  if ($result eq $l) { # OK
+    print " [OK]\n";
+  } else {             # NOK
+    $wrong++;
+    print "\n";
+    print STDERR "Warning on line $i: expected result from the example does not match\n";
+    print STDERR "                    the actual result of the expression.\n";
+    print STDERR "    Expected result: $l\n";
+    print STDERR "    Actual result:   $result\n";
+  }
+}
+
+1;
