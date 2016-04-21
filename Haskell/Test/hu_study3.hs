9d6356a3bac28c96ccc37c2d08204e2a9dc431fe
zirun zhu <zirunzhu@gmail.com>
2016-04-21 15:14:38 +0900

    revised hu_study3.hs

--- Haskell/Test/hu_study3.hs
+++ Haskell/Test/hu_study3.hs
@@ -7,7 +7,7 @@
 -}
 
 import GHC.Generics
-import Generics.BiGUL
+import Generics.BiGUL.Interpreter
 import Util
 import Generics.BiGUL.AST
 import Language.Haskell.TH as TH
@@ -15,11 +15,11 @@ import Generics.BiGUL.TH
 
 -- upFst:
 --  (a,b) <-> a
-upFst :: Eq a => BiGUL m (a,b) a
+upFst :: Eq a => BiGUL (a,b) a
 --upFst = Rearr RVar (EDir DVar `EProd` EConst ())
 --                   (Update (UVar Replace `UProd` UVar Skip))
 
-upFst = $(rearr [| \x -> (x,()) |]) $(update [p| (x,_) |] [d| x = Replace |])
+upFst = $(rearrV [| \x -> (x,()) |]) $(update [p| (x,()) |] [p| (x,_) |]  [d| x = Replace |])
 
 {-
 
@@ -32,11 +32,11 @@ Right (100,2)
 
 -- upSnd:
 --  (a,b) <-> b
-upSnd :: Eq b => BiGUL m (a,b) b
+upSnd :: Eq b => BiGUL (a,b) b
 --upSnd = Rearr RVar (EConst () `EProd` EDir DVar)
 --                   (Update (UVar Skip `UProd` UVar Replace))
 
-upSnd = $(rearr [|  \x -> ((),x) |]) $(update [p| (_,x) |] [d| x = Replace |])
+upSnd = $(rearrV [|  \x -> ((),x) |]) $(update [p| ((),x) |] [p| (_,x) |] [d| x = Replace |])
 
 {-
 
@@ -47,10 +47,10 @@ Right (1,200)
 
 -}
 
-upSwap :: (Eq a, Eq b) => BiGUL m (a,b) (b,a)
+upSwap :: (Eq a, Eq b) => BiGUL (a,b) (b,a)
 --upSwap = Rearr (RVar `RProd` RVar) (EDir (DRight DVar) `EProd` EDir (DLeft DVar)) Replace
 
-upSwap = $(rearr [| \(x,y) -> (y,x) |]) Replace
+upSwap = $(update [p| (x,y) |] [p| (y,x) |] [d| x=Replace; y = Replace |])
 
 {-
 
@@ -138,44 +138,62 @@ Right [1,2,3,100,5,6,7,8,9,10]
 -- uLefts
 --  [Left 1, Right 1, Left 3, Left 3, Right 2] <-> [Left 1, Left 3, Left 3]
 
-
---original uLefts
---uLefts :: (MonadError' ErrorInfo m, Eq a) => a -> BiGUL m [Either a a] [Either a a]
---uLefts a0 = CaseV [ CaseVBranch (PIn (PLeft (PConst ()))) $
---                      CaseS [ (return . all (not . isLeft), Normal Skip),
---                              (return . const True, Adaptive (\s v-> return (rmLefts s)))
---                            ],
---                    CaseVBranch (PIn (PRight (PProd PVar PVar))) $
---                      CaseS [ (\s -> return (s/=[] && hasLeftHead s),
---                                 Normal (Update (UElem (UVar Replace) (UVar (uLefts a0))))),
---                              (\s -> return (s/=[] && not (hasLeftHead s)),
---                                 Normal $ Rearr (RProd RVar RVar) (EProd (EConst ()) (EElem (EDir (DLeft DVar)) (EDir (DRight DVar))))
---                                                (Update (UElem (UVar Skip) (UVar (uLefts a0))))),
---                              (return . (==[]), Adaptive (\s -> return undefined))
---                            ]
+-- original uLefts (also workable)
+--uLefts :: (Eq a) => BiGUL [Either a a] [Either a a]
+--uLefts =
+--  Case [ ( (\_ v -> case v of [] -> True; _ -> False),
+--            Normal
+--            (Case [ ( (\s _ -> all (not . isLeft) s),
+--                      Normal
+--                        (RearrV (PIn (PLeft (PConst ())))  (EConst ()) Skip)
+--                        (\s -> all (not . isLeft) s) )
+--                  , ( (\_ _ -> True),
+--                      Adaptive (\s _ -> rmLefts s))
 --                  ]
+--            )
+--            (const True)
+--         )
+--       ,  ( (\_ v -> case v of _:_ -> True; _ -> False),
+--            Normal
+--            (Case [ ( (\s _ -> s /= [] && hasLeftHead s),
+--                      Normal ( RearrS (PIn (PRight $ (PLeft PVar) `PProd` PVar))
+--                                      (EDir (DLeft DVar) `EProd` EDir (DRight DVar))
+--                                      (RearrV (PIn (PRight $ (PLeft PVar) `PProd` PVar))
+--                                              (EDir (DLeft DVar) `EProd` EDir (DRight DVar))
+--                                              (Replace `Prod` uLefts))
+--                             )
+--                             (\s -> s /= [] && hasLeftHead s) )
+--                  , ( (\s _ -> s /= [] && not (hasLeftHead s)),
+--                      Normal ( RearrS (PIn $ PRight (PRight PVar `PProd` PVar))
+--                                      (ERight (EDir (DLeft DVar)) `EProd` EDir (DRight DVar))
+--                                      (RearrV (PIn $ PRight (PVar `PProd` PVar))
+--                                              (EConst () `EProd` (EIn $ ERight $ EDir (DLeft DVar) `EProd` EDir (DRight DVar)))
+--                                              (Skip `Prod` uLefts))
+--                             )
+--                             (\s -> s /= [] && not (hasLeftHead s)) )
+--                  , (\s _ -> s == [], Adaptive (\_ _-> undefined))
+--                  ])
+--            (const True)
 
+--          )
+--        ]
 
-
-uLefts :: (MonadError' ErrorInfo m, Eq a) => BiGUL m [Either a a] [a]
+uLefts :: (Eq a) => BiGUL [Either a a] [Either a a]
 uLefts = Case [ $(normalV [p| [] |]) $
-                  $(rearr [| \[] -> () |]) $
-                    Case [ $(normalS [| all (not . isLeft) |])
-                             Skip
-                         , $(adaptiveS [p| _ |]) $
-                             \s _ -> rmLefts s
+                  $(rearrV [| \[] -> () |]) $
+                    Case [ $(normalS [| all (not . isLeft) |]) Skip
+                         , $(adaptiveS [p| _ |]) (\s _ -> rmLefts s)
                          ]
               , $(normalV [p| _ : _ |]) $
-                   $(rearr [| \(x:xs) -> (x,xs) |]) $
-                     (Case [ $(normalS [p| Left  _ : _ |]) $
+                     (Case [ $(normalS [p| Left _ : _ |]) $
                                $(update [p| Left x : xs |]
-                                        [d| x  = Replace
-                                            xs = uLefts |])
+                                        [p| Left x : xs |]
+                                        [d| x  = Replace; xs = uLefts |])
                            , $(normalS [p| Right _ : _ |]) $
-                               $(rearr [| \(x,xs) -> ((), x:xs) |]) $
-                                 $(update [p| _ : xs |] [d| xs = uLefts |])
+                               $(rearrV [| \(x:xs) -> ((), x:xs) |]) $
+                                 $(update [p| ((), xs) |] [p| _ : xs |] [d| xs = uLefts |])
                            , $(adaptiveS [p| [] |]) $
-                               \_ _ -> [Left (error "uLefts")]
+                               \_ _ -> [Left undefined]
                            ])
               ]
 
@@ -188,13 +206,13 @@ isLeft _ = False
 
 {-
 
-*Main> testGet (uLefts (-1)) [Left 1, Right 1, Left 3, Left 3, Right 2]
+*Main> get uLefts [Left 1, Right 1, Left 3, Left 3, Right 2]
 Right [Left 1,Left 3,Left 3]
-*Main> testPut (uLefts (-1)) [Left 1, Right 1, Left 3, Left 3, Right 2] []
+*Main> put uLefts [Left 1, Right 1, Left 3, Left 3, Right 2] []
 Right [Right 1,Right 2]
-*Main> testPut (uLefts (-1)) [Left 1, Right 1, Left 3, Left 3, Right 2] [Left 100]
+*Main> put uLefts [Left 1, Right 1, Left 3, Left 3, Right 2] [Left 100]
 Right [Left 100,Right 1,Right 2]
-*Main> testPut (uLefts (-1)) [Left 1, Right 1, Left 3, Left 3, Right 2] [Left 100, Left 200, Left 300, Left 400]
+*Main> put uLefts [Left 1, Right 1, Left 3, Left 3, Right 2] [Left 100, Left 200, Left 300, Left 400]
 Right [Left 100,Right 1,Left 200,Left 300,Right 2,Left 400]
 
 -}
@@ -203,12 +221,12 @@ Right [Left 100,Right 1,Left 200,Left 300,Right 2,Left 400]
 -- rmLeftTags
 --  [Left 1, Left 2, Left 3] <-> [1,2,3]
 
-rmLeftTags :: (Eq a, Show a, MonadError' ErrorInfo m) => BiGUL m [Either a a] [a]
+rmLeftTags :: (Eq a, Show a) => BiGUL [Either a a] [a]
 rmLeftTags = mapU uLeft
 
-uLeft :: (Eq a, MonadError' ErrorInfo m) => BiGUL m (Either a a) a
-uLeft = Case [ $(normalS [p| Left _ |]) $ $(update [p| Left x |] [d| x = Replace |])
-             , $(normalS [p| _      |]) $ failMsg "rmLeftTags: any element in the source should be a left value."
+uLeft :: (Eq a) => BiGUL (Either a a) a
+uLeft = Case [ $(normalS [p| Left _ |]) $ $(update [p| x |] [p| Left x |] [d| x = Replace |])
+             , $(normalS [p| _      |]) $ Fail "rmLeftTags: any element in the source should be a left value."
              ]
 
 {-
