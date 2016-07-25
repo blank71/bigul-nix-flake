d91425c2159a5b2df7bad4454ff1393fc7a4015f
Zhenjiang <zhenjiangh@gmail.com>
2016-07-25 08:42:13 +0100

    minor

--- SummerSchool16/test.hs
+++ SummerSchool16/test.hs
@@ -1,4 +1,9 @@
+{-# LANGUAGE FlexibleContexts, TemplateHaskell, TypeFamilies #-}
 import Generics.BiGUL
 import Generics.BiGUL.Interpreter
 import Generics.BiGUL.TH
 import Generics.BiGUL.Lib
+
+-- hello: _ <-> Hello!
+hello :: Show a => BiGUL a String
+hello = Skip (\_ -> "Hello!")
