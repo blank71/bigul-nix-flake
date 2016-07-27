db7c25292fe97b6154e5feaeeb2364b9edb7efaa
Zhenjiang <zhenjiangh@gmail.com>
2016-07-27 03:43:21 +0100

    added the footnote to the paper, added the slides, and restructured the directory

--- SummerSchool16/paper/test.hs
+++ SummerSchool16/paper/test.hs
@@ -7,3 +7,13 @@ import Generics.BiGUL.Lib
 -- hello: _ <-> Hello!
 hello :: Show a => BiGUL a String
 hello = Skip (\_ -> "Hello!")
+
+
+pHead' :: Show s
+       => BiGUL [s] s
+pHead' = Case [
+     $(normal [| \s v -> not (null s) |] [| not . null |])
+       ==> pHead,
+     $(adaptive [| \s v -> null s |])
+       ==> \s v -> [v]
+     ]
