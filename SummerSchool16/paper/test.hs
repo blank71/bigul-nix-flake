e3231dfc3e5706ad3bae606971ba7924b715a533
Zhenjiang <zhenjiangh@gmail.com>
2017-01-17 15:42:18 +0900

    added comments to the current version

--- SummerSchool16/paper/test.hs
+++ SummerSchool16/paper/test.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE FlexibleContexts, TemplateHaskell, TypeFamilies #-}
 import Generics.BiGUL
-import Generics.BiGUL.Interpreter
+--import Generics.BiGUL.Interpreter
 import Generics.BiGUL.TH
 import Generics.BiGUL.Lib
 
