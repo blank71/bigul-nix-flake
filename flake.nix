a8adc2d061e0bd8225e77ddbc8b29f8a4099e311
blank71 <8blank71@gmail.com>
2026-06-24 11:41:50 +0900

    feat: ghc-8.6.5
    % stack ls dependencies
    BiGUL 1.1.0.1
    array 0.5.3.0
    base 4.12.0.0
    containers 0.6.0.1
    deepseq 1.4.4.0
    ghc-boot-th 8.6.5
    ghc-prim 0.5.3
    integer-gmp 1.0.2.0
    mtl 2.2.2
    pretty 1.1.3.6
    rts 1.0
    syb 0.7.1
    template-haskell 2.14.0.0
    th-extras 0.0.0.4
    transformers 0.5.6.2

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 # https://docs.haskellstack.org/en/v3.9.3/topics/nix_integration/#supporting-both-nix-and-non-nix-developers
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.nixpkgs-release.url = "github:NixOS/nixpkgs/release-20.03";
+  inputs.nixpkgs-release.url = "github:NixOS/nixpkgs/release-20.09";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs =
@@ -20,10 +20,10 @@
 
         # Use the default GHC from nixpkgs to avoid removed compiler sets.
         # If you need a specific GHC, switch to pkgs.haskell.packages.ghcXYZ.
-        hPkgs = haskell.packages.ghc844;
+        hPkgs = haskell.packages.ghc865;
 
         myDevTools = [
-          # For install ghc-8.4.4
+          # For install ghc
           pkgs-release.gmp
           pkgs-release.libffi
           pkgs-release.ncurses5
@@ -33,8 +33,8 @@
           # hPkgs.ormolu # Haskell formatter
           # hPkgs.hlint # Haskell codestyle checker
           # hPkgs.hoogle # Lookup Haskell documentation
-          # hPkgs.haskell-language-server # LSP server for editor
-          # hPkgs.implicit-hie # auto generate LSP hie.yaml file from cabal
+          hPkgs.haskell-language-server # LSP server for editor
+          hPkgs.implicit-hie # auto generate LSP hie.yaml file from cabal
           # hPkgs.retrie # Haskell refactoring tool
           # hPkgs.cabal-install
           stack-wrapped
